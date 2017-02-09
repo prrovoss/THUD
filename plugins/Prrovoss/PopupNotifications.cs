493d6a9fc100817664ec30fd1f5e24fc736e3630
prrovoss <prrovoss@freenet.de>
2017-02-10 00:17:28 +0100

    Update PopupNotifications.cs

--- plugins/Prrovoss/PopupNotifications.cs
+++ plugins/Prrovoss/PopupNotifications.cs
@@ -1,5 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
+using System;
 
 using Turbo.Plugins.Default;
 
@@ -8,7 +7,6 @@ namespace Turbo.Plugins.Prrovoss
 
     public class PopupNotifications : BasePlugin
     {
-        public List<Popup> Popups { get; set; }
         public TopLabelWithTitleDecorator PopupDecorator { get; set; }
         public float RatioX { get; set; }
         public float RatioY { get; set; }
@@ -16,33 +14,27 @@ namespace Turbo.Plugins.Prrovoss
         public float RatioH { get; set; }
         public float VerticalGap { get; set; }
 
-        public class Popup
+        public class Popup : IQueueItem
         {
             public string Text { get; set; }
             public string Title { get; set; }
             public string Hint { get; set; }
-            public long EndMilli { get; set; }
-            public Popup(string text, string title, long endMilli, string hint)
+            public DateTime QueuedOn { get; private set; }
+            public TimeSpan LifeTime { get; private set; }
+            public Popup(string text, string title, TimeSpan lifetime, string hint)
             {
                 this.Text = text;
                 this.Title = title;
-                this.EndMilli = endMilli;
+                this.LifeTime = lifetime;
                 this.Hint = hint;
+                this.QueuedOn = DateTime.Now;
             }
         }
 
 
-        public bool Show(string text, string title, int duration, string hint = null)
+        public void Show(string text, string title, int duration, string hint = null)
         {
-            if (Popups.Count < 50)
-            {
-                Popups.Add(new Popup(text, title, Hud.Game.CurrentRealTimeMilliseconds + duration, hint));
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            Hud.Queue.AddItem(new Popup(text, title, new TimeSpan(0, 0, 0, 0, duration), hint));
         }
 
         public PopupNotifications()
@@ -67,34 +59,25 @@ namespace Turbo.Plugins.Prrovoss
             RatioW = 0.18f;
             RatioH = 0.05f;
             VerticalGap = 1.20f;
-
-
-            Popups = new List<Popup>();
         }
 
         public override void PaintWorld(WorldLayer layer)
         {
             int ctr = 0;
 
-
             var w = Hud.Window.Size.Height * RatioW;
             var h = Hud.Window.Size.Height * RatioH;
             var x = Hud.Window.Size.Width * RatioX;
             var y = Hud.Window.Size.Height * RatioY;
 
-            Popup p;
-            while ((ctr < Popups.Count) && (ctr < 13))
+            foreach (Popup p in Hud.Queue.GetItems<Popup>())
             {
-                p = Popups.ElementAt(ctr);
-                if (p.EndMilli < Hud.Game.CurrentRealTimeMilliseconds)
-                {
-                    Popups.Remove(p);
-                }
-                else
+                if (ctr < 13)
                 {
                     PopupDecorator.Paint(x, y - ctr * (h * VerticalGap), w, h, p.Text, p.Title, p.Hint);
                     ctr++;
                 }
+                else { return; }
             }
 
         }
